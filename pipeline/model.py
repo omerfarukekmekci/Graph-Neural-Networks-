c2b932e3933f24c98efa66145b99fd7615f36a9c
omerfarukekmekci <omerfarukekmekci2004@gmail.com>
2025-12-16 16:56:46 +0300

    added edge decoder class and netagive sampling tool

--- pipeline/model.py
+++ pipeline/model.py
@@ -1,5 +1,5 @@
 import torch
-from torch.nn import Linear
+import torch.nn as nn
 from torch_geometric.data import Data
 from torch_geometric.nn import SAGEConv
 
@@ -16,6 +16,27 @@ class GraphEncoder(torch.nn.Module):
         x = self.conv(x, edge_index)
         x = torch.relu(x)
         return x
+
+
+class EdgeDecoder(nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+
+        self.lin1 = nn.Linear(hidden_channels * 2, hidden_channels)
+        self.lin2 = nn.Linear(hidden_channels, 1)
+
+    def forward(self, z, edge_index):
+        src, dst = edge_index
+        z_src = z[src]
+        z_dst = z[dst]
+
+        edge_feat = torch.cat(z_src, z_dst, dim=1)
+
+        intermediate1 = self.lin1(edge_feat)
+        intermediate2 = torch.relu(intermediate1)
+        score = self.lin2(intermediate2)
+
+        return score
 
 
 def build_graph():
@@ -42,3 +63,20 @@ def build_graph():
     graph = Data(x=x, edge_index=edge_index).to(device)
 
     return graph
+
+
+def negative_sampling(edge_index, num_nodes, num_negatives):
+    existing_edges = set((int(u), int(v)) for u, v in edge_index.t().tolist())
+
+    neg_edges = []
+
+    while len(neg_edges) < num_negatives:
+        u = torch.randint(0, num_nodes, (1,)).item()
+        v = torch.randint(0, num_nodes, (1,)).item()
+
+        if (u, v) in existing_edges:
+            continue
+
+        neg_edges.append([u, v])
+
+    return torch.tensor(neg_edges, dtype=torch.long).t()
