bc9f78a0d73ca54c980a7d41f752bcd923cfc03f
omerfarukekmekci <omerfarukekmekci2004@gmail.com>
2026-01-26 16:33:54 +0300

    edge decoder implemented

--- simple_graph.py
+++ simple_graph.py
@@ -18,14 +18,32 @@ class MyGraphModel(torch.nn.Module):
         x = self.conv2(x, edge_index)
         return x
 
+class EdgeDecoder(torch.nn.Module):
+    def __init__(self, hidden_channels):
+        super().__init__()
+        self.lin1 = Linear(hidden_channels * 2, hidden_channels)
+        self.lin2 = Linear(hidden_channels, 1)
 
-x = torch.tensor([[1.0, 2.0], [2.0, 3.0], [3.0, 5.0], [5.0, 7.0]], dtype=torch.float)
+    def forward(self, z, edge_index):
+        src, dst = edge_index
+        z_src = z[src]
+        z_dst = z[dst]
+
+        edge_feat = torch.cat([z_src, z_dst], dim=1)
+
+        x = self.lin1(edge_feat)
+        x = torch.relu(x)
+        score = self.lin2(x)
+
+        return score
+
+
+x = torch.tensor([[1.0, 2.0], [7.0, 11.0], [3.0, 5.0], [15.0, 23.0], [31.0, 47.0]], dtype=torch.float)
 edge_index = (
     torch.tensor(
-        [[0, 1], [1, 0], [0, 2], [2, 0], [2, 1], [1, 2], [2, 3], [3, 2]],
+        [[0, 2, 1, 2, 1, 3], [2, 0, 2, 1, 3, 1]],
         dtype=torch.long,
     )
-    .t()
     .contiguous()
 )
 
@@ -40,3 +58,11 @@ model = MyGraphModel(
 out = model(graph.x, graph.edge_index)
 
 print(graph)
+
+decoder = EdgeDecoder(hidden_channels=64).to(device)
+scores = decoder(out, graph.edge_index)
+print("Edge scores:", scores)
+
+test_edge = torch.tensor([[3], [4]], dtype=torch.long).to(device)
+prediction = decoder(out, test_edge)
+print(f"Score for edge 3->4: {prediction.item()}")
